68ad65c3f631b406af09f9cb742e432982794a15
Ray Strode <rstrode@redhat.com>
2007-05-08 17:57:15 -0400

    Add stub implementation to main.c Previously the very empty project didn't build because it didn't contain a definition for main().  Add a stub implementation to get things building

---
+++ src/main.c
@@ -0,0 +1,29 @@
+/* vim: ts=4 sw=2 expandtab autoindent cindent 
+ * main.c - graphical boot splash screen
+ *
+ * Copyright (C) 2007 Red Hat, Inc
+ * All rights reserved.
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this; see the file COPYING.  If not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * Written by: Ray Strode <rstrode@redhat.com>
+ */
+int
+main (int    argc,
+      char **argv)
+{
+  return 0;
+}

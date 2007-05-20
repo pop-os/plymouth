74f0ebf213b567050b2386ad5e11079a5984978d
Ray Strode <rstrode@redhat.com>
2007-05-19 20:16:14 -0400

    fix up string array duplicating functions to compile add new function to close all open fds

--- src/ply-utils.c
+++ src/ply-utils.c
@@ -3,11 +3,20 @@
 #include "ply-utils.h"
 
 #include <assert.h>
+#include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <limits.h>
 #include <poll.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/resource.h>
+#include <sys/time.h>
 #include <sys/types.h>
+
+#ifndef PLY_OPEN_FILE_DESCRIPTORS_DIR
+#define PLY_OPEN_FILE_DESCRIPTORS_DIR "/proc/self/fd"
+#endif
 
 bool 
 ply_open_unidirectional_pipe (int *sender_fd,
@@ -110,9 +119,9 @@ ply_copy_string_array (const char * const *array)
 
   for (i = 0; array[i] != NULL; i++);
 
-  copy = calloc (i + 1, char *);
+  copy = calloc (i + 1, sizeof (char *));
 
-  for (i = 0; array[i] != NULL, i++)
+  for (i = 0; array[i] != NULL; i++)
     copy[i] = strdup (array[i]);
 
   return copy;
@@ -134,4 +143,89 @@ ply_free_string_array (char **array)
 
   free (array);
 }
+
+static int
+ply_get_max_open_fds (void)
+{
+  struct rlimit open_fd_limit;
+
+  if (getrlimit (RLIMIT_NOFILE, &open_fd_limit) < 0) 
+    return -1;
+
+  if (open_fd_limit.rlim_cur == RLIM_INFINITY) 
+    return -1;
+
+  return (int) open_fd_limit.rlim_cur;
+}
+
+static bool
+ply_close_open_fds (void)
+{
+  DIR *dir;
+  struct dirent *entry;
+  int fd, opendir_fd;
+
+  opendir_fd = -1;
+  dir = opendir (PLY_OPEN_FILE_DESCRIPTORS_DIR);
+
+  if (dir == NULL)
+    return false;
+
+  while ((entry = readdir (dir)) != NULL) 
+    {
+      long filename_as_number;
+      char *byte_after_number;
+
+      errno = 0;
+      if (entry->d_name[0] == '.')
+        continue;
+
+      fd = -1;
+      filename_as_number = strtol (entry->d_name, &byte_after_number, 10);
+
+      if (byte_after_number != NULL)
+        continue;
+
+      if ((*byte_after_number != '\0') ||
+          (filename_as_number < 0) ||
+          (filename_as_number > INT_MAX)) 
+        return false;
+
+      fd = (int) filename_as_number;
+
+      if (fd != opendir_fd)
+        close (fd);
+    }
+
+  assert (entry == NULL);
+  closedir (dir);
+
+  return true;
+}
+
+void 
+ply_close_all_fds (void)
+{
+  int max_open_fds, fd;
+
+  max_open_fds = ply_get_max_open_fds ();
+
+  /* if there isn't a reported maximum for some
+   * reason, then open up /proc/self/fd and close
+   * the ones we can find.  If that doesn't work
+   * out, then just bite the bullet and close the
+   * entire integer range
+   */
+  if (max_open_fds < 0)
+    {
+      if (ply_close_open_fds ())
+        return;
+
+      max_open_fds = INT_MAX;
+    }
+
+  else for (fd = 0; fd < max_open_fds; fd++) 
+    close (fd);
+}
+
 /* vim: set ts=4 sw=4 expandtab autoindent cindent cino={.5s,(0: */
--- src/ply-utils.h
+++ src/ply-utils.h
@@ -48,6 +48,7 @@ bool ply_fd_has_data (int fd);
 bool ply_fd_can_take_data (int fd);
 char **ply_copy_string_array (const char * const *array);
 void ply_free_string_array (char **array);
+void ply_close_all_fds (void);
 #endif
 
 #endif /* PLY_UTILS_H */

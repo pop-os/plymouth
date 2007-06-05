94efb1940a8cf1af78d7a7f395ed71b6f0cbce81
Ray Strode <rstrode@redhat.com>
2007-06-05 00:42:38 -0400

    add the start of boot status tracking to plymouth

--- src/main.c
+++ src/main.c
@@ -21,10 +21,14 @@
  */
 #include "config.h"
 
+#include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <sysexits.h>
 
+#include "ply-boot-server.h"
 #include "ply-event-loop.h"
 #include "ply-logger.h"
 #include "ply-terminal-session.h"
@@ -33,16 +37,62 @@
 typedef struct
 {
   ply_event_loop_t *loop;
+  ply_boot_server_t *boot_server;
   ply_terminal_session_t *session;
 } state_t;
 
 static void
 on_session_finished (state_t *state)
 {
-  ply_log ("Session finished...exiting logger\n");
+  ply_log ("\nSession finished...exiting logger\n");
   ply_flush_log ();
-  ply_event_loop_exit (state->loop, 0);
+  //ply_event_loop_exit (state->loop, 0);
 } 
+
+static void 
+on_update (state_t     *state,
+           const char  *status)
+{
+  ply_log ("\nSession now at boot status '%s'\n", status);
+  ply_flush_log ();
+}
+
+static void
+on_system_initialized (state_t *state)
+{
+  ply_log ("\nGot told system is initialized...\n");
+  ply_flush_log ();
+}
+
+static void
+on_quit (state_t *state)
+{
+  ply_log ("\nGot quit request, quitting...\n");
+  ply_flush_log ();
+  //ply_event_loop_exit (state->loop, 0);
+}
+
+static ply_boot_server_t *
+start_boot_server (state_t *state)
+{
+  ply_boot_server_t *server;
+
+  server = ply_boot_server_new ((ply_boot_server_update_handler_t) on_update,
+                                (ply_boot_server_system_initialized_handler_t) on_system_initialized,
+                                (ply_boot_server_quit_handler_t) on_quit,
+                                state);
+
+  if (!ply_boot_server_listen (server))
+    {
+      ply_save_errno ();
+      ply_boot_server_free (server);
+      ply_restore_errno ();
+    }
+
+  ply_boot_server_attach_to_event_loop (server, state->loop);
+
+  return server;
+}
 
 static ply_terminal_session_t *
 spawn_session (state_t  *state, 
@@ -86,6 +136,7 @@ main (int    argc,
     }
 
   state.loop = ply_event_loop_new ();
+  state.boot_server = start_boot_server (&state); 
   state.session = spawn_session (&state, argv + 1);
 
   if (state.session == NULL)

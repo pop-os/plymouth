ab84465fa71f47044784d0d4f7d90c8016a7994e
Ray Strode <rstrode@redhat.com>
2007-11-01 01:03:55 -0400

    Fix up autogen to error out if it hits a problem and rerun autoreconf after intltoolize.

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 (cd $(dirname $0);
- autoreconf --install --symlink;
- intltoolize --force;
+ autoreconf --install --symlink &&
+ intltoolize --force &&
+ autoreconf --install --symlink &&
  ./configure --enable-maintainer-mode $@)

a36d61d12ace996aba4c737582bf04063004ca1b
Charlie Brej <cbrej@cs.man.ac.uk>
2009-07-03 15:53:43 +0100

    [script] Add GPL 2+ licence headers to all .c and .h files

--- src/plugins/splash/script/plugin.c
+++ src/plugins/splash/script/plugin.c
@@ -1,7 +1,7 @@
 /* plugin.c - boot script plugin
  *
  * Copyright (C) 2007, 2008 Red Hat, Inc.
- *                     2008 Charlie Brej <cbrej@cs.man.ac.uk>
+ *               2008, 2009 Charlie Brej <cbrej@cs.man.ac.uk>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

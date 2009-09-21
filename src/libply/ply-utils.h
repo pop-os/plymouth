069839a58b971bce8e3904b1c1237bf78e37318f
Ray Strode <rstrode@redhat.com>
2009-09-21 13:49:43 -0400

    [utils] Add ply_round_to_multiple macro
    We'll need something like this for rowstride calculation

--- src/libply/ply-utils.h
+++ src/libply/ply-utils.h
@@ -46,6 +46,9 @@ typedef void (* ply_module_function_t) (void);
 typedef intptr_t ply_daemon_handle_t;
 
 #ifndef PLY_HIDE_FUNCTION_DECLARATIONS
+
+#define ply_round_to_multiple(n, m) (((n) + (((m) - 1))) & ~((m) - 1))
+
 bool ply_open_unidirectional_pipe (int *sender_fd,
                                    int *receiver_fd);
 int ply_connect_to_unix_socket (const char *path,

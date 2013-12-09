c66c207d2e25ae77d118a6037db35454678a49dc
Ray Strode <rstrode@redhat.com>
2013-12-09 13:13:38 -0500

    device-manager: add support for udev device enumeration
    At the moment, we hardcode /dev/dri/card0 for the DRM device.
    This works well enough in the lion's share of cases, but there are cases
    where it falls over.  Namely, machines with multiple GPUs, such as
    optimus hardware, sometimes end up with the kernel fb console going to
    /dev/dri/card1.
    
    Rather than trying /dev/dri/card0 then /dev/dri/card1 etc in succession,
    this commit, instead, adds support for querying udev for the
    information.

--- src/libply-splash-core/ply-device-manager.h
+++ src/libply-splash-core/ply-device-manager.h
@@ -26,7 +26,8 @@
 typedef enum
 {
   PLY_DEVICE_MANAGER_FLAGS_NONE                   = 0,
-  PLY_DEVICE_MANAGER_FLAGS_IGNORE_SERIAL_CONSOLES = 1 << 0
+  PLY_DEVICE_MANAGER_FLAGS_IGNORE_SERIAL_CONSOLES = 1 << 0,
+  PLY_DEVICE_MANAGER_FLAGS_IGNORE_UDEV            = 1 << 1
 } ply_device_manager_flags_t;
 
 typedef struct _ply_device_manager ply_device_manager_t;

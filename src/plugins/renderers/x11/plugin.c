63148b20a1f64864b988d9de78b819e4d3232076
Ray Strode <rstrode@redhat.com>
2017-01-18 13:09:53 -0500

    x11: implement get_display_name
    For completeness, implement get_display_name in the x11 renderer too.

--- src/plugins/renderers/x11/plugin.c
+++ src/plugins/renderers/x11/plugin.c
@@ -170,6 +170,12 @@ open_device (ply_renderer_backend_t *backend)
         return true;
 }
 
+static const char *
+get_device_name (ply_renderer_backend_t *backend)
+{
+        return gdk_display_get_name (gdk_display_get_default ());
+}
+
 static void
 close_device (ply_renderer_backend_t *backend)
 {
@@ -539,7 +545,8 @@ ply_renderer_backend_get_interface (void)
                 .get_input_source             = get_input_source,
                 .open_input_source            = open_input_source,
                 .set_handler_for_input_source = set_handler_for_input_source,
-                .close_input_source           = close_input_source
+                .close_input_source           = close_input_source,
+                .get_device_name              = get_device_name
         };
 
         return &plugin_interface;
